dc9590b9e1c528b57674dc41baafe0566db5681f
kamelelrefaie <kamelelrifai@hotmail.com>
2022-04-21 00:32:17 +0200

    complete SearchFragment.kt

--- app/src/main/java/com/example/metroid/ui/view/main_Cycle/SearchFragment.kt
+++ app/src/main/java/com/example/metroid/ui/view/main_Cycle/SearchFragment.kt
@@ -1,11 +1,14 @@
 package com.example.metroid.ui.view.main_Cycle
 
+import android.opengl.Visibility
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.metroid.R
+import com.example.metroid.databinding.FragmentReservationBinding
+import com.example.metroid.databinding.FragmentSearchBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,9 +36,13 @@ class SearchFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        val fragmentSearchBinding
+                : FragmentSearchBinding =
+            FragmentSearchBinding.inflate(inflater)
+
+
+        return fragmentSearchBinding.root
     }
 
     companion object {
